7230885569536ed72c963ce5c1640be1fc7efce0
a.kotomcev <kotgreen@mail.ru>
2026-04-20 13:10:48 +0300

    инициализация проекта

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        Contact page
+    </x-slot:heading>
+
+    <h2>contact page</h2>
+</x-layout>
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs page
+    </x-slot:heading>
+
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                    <strong>{{ $job['title'] }}-</strong> {{ $job['salary'] }}₽
+                </div>
+            </a>
+        @endforeach
+    </div>
+</x-layout>
